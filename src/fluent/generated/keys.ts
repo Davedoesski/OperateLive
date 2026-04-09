eeaffa25c1c7f34bcb7ddc459a23ae40d0c9f6db
Dave Smith <dsmith3@deloitte.com.au>
2026-04-09 11:15:01 +1000

    V2

--- src/fluent/generated/keys.ts
+++ src/fluent/generated/keys.ts
@@ -44,11 +44,36 @@ declare global {
                         }
                     },
                     {
+                        table: 'sys_dictionary'
+                        id: '29e6e0ac8b9e47ebaee38d8c43af900e'
+                        key: {
+                            name: 'sn_customerservice_case'
+                            element: 'x_dttm_customer_0_additional_time_approved'
+                        }
+                    },
+                    {
+                        table: 'sys_dictionary'
+                        id: '365b486d47f4432eba4d3804663d6cd3'
+                        key: {
+                            name: 'sn_customerservice_case'
+                            element: 'x_dttm_customer_0_total_approved_time'
+                        }
+                    },
+                    {
                         table: 'sys_security_acl_role'
                         id: '3d2da781d8a34b2c93018a7ad678340b'
                         key: {
                             sys_security_acl: 'e3923e2ddf8c4b0ea6cc6ac9c9185f0f'
                             sys_user_role: '2831a114c611228501d4ea6c309d626d'
+                        }
+                    },
+                    {
+                        table: 'sys_documentation'
+                        id: '3e5520bf43954fa6a661e9259415d054'
+                        key: {
+                            name: 'sn_customerservice_case'
+                            element: 'x_dttm_customer_0_total_approved_time'
+                            language: 'en'
                         }
                     },
                     {
@@ -60,11 +85,26 @@ declare global {
                         }
                     },
                     {
+                        table: 'sys_dictionary'
+                        id: '514771df5f5b4fe3aee0c39ea099936c'
+                        key: {
+                            name: 'sn_customerservice_case'
+                            element: 'x_dttm_customer_0_additional_time_requested'
+                        }
+                    },
+                    {
                         table: 'sys_security_acl_role'
                         id: '5e6ee63440e0441cbd197045715a0d49'
                         key: {
                             sys_security_acl: '7ded59d2d23d4b9e8d7623ffe5b787ff'
                             sys_user_role: '259e90f6c3223100d6d210c422d3aefb'
+                        }
+                    },
+                    {
+                        table: 'sys_db_object'
+                        id: '5fc39b7156784a23aa35ff45db435d87'
+                        key: {
+                            name: 'sn_customerservice_case'
                         }
                     },
                     {
@@ -84,11 +124,37 @@ declare global {
                         }
                     },
                     {
+                        table: 'sys_dictionary'
+                        id: '6fe4e8218bec45758dbfcb5e95f3d22b'
+                        key: {
+                            name: 'sn_customerservice_case'
+                            element: 'x_dttm_customer_0_approved_effort'
+                        }
+                    },
+                    {
+                        table: 'sys_documentation'
+                        id: '7442d7b4fa0143848a55a6a8e30fbd1e'
+                        key: {
+                            name: 'sn_customerservice_case'
+                            element: 'x_dttm_customer_0_additional_time_requested'
+                            language: 'en'
+                        }
+                    },
+                    {
                         table: 'sys_security_acl_role'
                         id: '80af8c3f4a224ae4bda5877523e4ff0b'
                         key: {
                             sys_security_acl: '7ded59d2d23d4b9e8d7623ffe5b787ff'
                             sys_user_role: '2831a114c611228501d4ea6c309d626d'
+                        }
+                    },
+                    {
+                        table: 'sys_documentation'
+                        id: '8633ed5442b340cd953dc1049aefee29'
+                        key: {
+                            name: 'sn_customerservice_case'
+                            element: 'x_dttm_customer_0_effort_to_date'
+                            language: 'en'
                         }
                     },
                     {
@@ -108,11 +174,45 @@ declare global {
                         }
                     },
                     {
+                        table: 'sys_dictionary'
+                        id: 'af3d3f793ff445ab9b614d8948f24833'
+                        key: {
+                            name: 'sn_customerservice_case'
+                            element: 'x_dttm_customer_0_effort_to_date'
+                        }
+                    },
+                    {
                         table: 'sys_security_acl_role'
                         id: 'c20306882df34491963cf0144d8be0b1'
                         key: {
                             sys_security_acl: '52861ea530954191a1032c533ce4ef69'
                             sys_user_role: '2831a114c611228501d4ea6c309d626d'
+                        }
+                    },
+                    {
+                        table: 'sys_documentation'
+                        id: 'd85a74112f504bd1813b69a7f83d58dd'
+                        key: {
+                            name: 'sn_customerservice_case'
+                            element: 'x_dttm_customer_0_additional_time_approved'
+                            language: 'en'
+                        }
+                    },
+                    {
+                        table: 'sys_documentation'
+                        id: 'e804d632355a49cb9f52306c30d2d59d'
+                        key: {
+                            name: 'sn_customerservice_case'
+                            element: 'x_dttm_customer_0_approved_effort'
+                            language: 'en'
+                        }
+                    },
+                    {
+                        table: 'sys_dictionary'
+                        id: 'ee196fdeddd1494fa966e5bf16bc0434'
+                        key: {
+                            name: 'sn_customerservice_case'
+                            element: 'NULL'
                         }
                     },
                 ]
